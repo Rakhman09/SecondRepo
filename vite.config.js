37c4b7a8d084138bae0889b71b0bcb94edfd265a
Rakhman09 <rakhman.zholdasbek@gmail.com>
2024-09-25 17:11:26 +0500

    fix: fix vercel deploy error

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src', // Optional: add an alias for easier imports
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: '/src/main.jsx', // Ensure Rollup knows the entry point
-    },
-  },
-});
+})
+
